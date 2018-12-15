e92cf6f6b3f821e8f66645dc957a47afd8917093
Dusan Svilarkovic <18049803+dsvilarkovic@users.noreply.github.com>
2018-12-15 16:31:38 +0000

    Ispravka konekcije za rad od kuce

--- URMA/src/controler/handlers/DBHandler.java
+++ URMA/src/controler/handlers/DBHandler.java
@@ -56,6 +56,41 @@ public class DBHandler implements IHandler {
 		System.out.println(sql);
 		
 		PreparedStatement pstmt;
+		
+
+//		/**
+//		 * @author Dusan
+//		 */
+//		String urlEduroam = "jdbc:jtds:sqlserver://192.168.77.230/psw-2018-tim7-1";
+//		String urlHome = "jdbc:jtds:sqlserver://147.91.175.155/psw-2018-tim7-1";
+//		String [] urls = {urlEduroam, urlHome};
+//		boolean stop_other_connections = false; //sluzi za proveru da li treba isprobavati dalje konektovanje
+//		
+//		for (String url : urls) {
+//			try {
+//				conn = DriverManager.getConnection(url,"psw-2018-tim7-1","tim7-19940718");
+//				pstmt = conn.prepareStatement(sql);
+//				pstmt.execute();
+//				pstmt.close();
+//				stop_other_connections = true; 
+//			} catch (SQLException e) {
+////				String text = "<html>No such table <strong style=\"color: red;\">"+ table.getTitle() +"</strong> in database or no connection</html>";
+////				JOptionPane.showMessageDialog(null, text, "INVALID SQL", JOptionPane.ERROR_MESSAGE);
+//				e.printStackTrace();
+//			}			
+//			if(stop_other_connections) {
+//				break;
+//			}
+//		}
+		
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:jtds:sqlserver://147.91.175.155/psw-2018-tim7-1","psw-2018-tim7-1","tim7-19940718");
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 		try {
 			conn = DriverManager.getConnection("jdbc:jtds:sqlserver://147.91.175.155/psw-2018-tim7-1","psw-2018-tim7-1","tim7-19940718");
 			pstmt = conn.prepareStatement(sql);
@@ -81,7 +116,9 @@ public class DBHandler implements IHandler {
 		
 		PreparedStatement pstmt;
 		try {
-			conn = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.77.230/psw-2018-tim7-1","psw-2018-tim7-1","tim7-19940718");
+//			conn = DriverManager.getConnection("jdbc:jtds:sqlserver://192.168.77.230/psw-2018-tim7-1","psw-2018-tim7-1","tim7-19940718");
+			conn = DriverManager.getConnection("jdbc:jtds:sqlserver://147.91.175.155/psw-2018-tim7-1","psw-2018-tim7-1","tim7-19940718");
+
 			pstmt = conn.prepareStatement(sql);
 			ResultSet rset = pstmt.executeQuery();
 			
@@ -105,6 +142,64 @@ public class DBHandler implements IHandler {
 			JOptionPane.showMessageDialog(null, text, "INVALID SQL", JOptionPane.ERROR_MESSAGE);
 			return null;
 		}
+		
+		
+		
+//		/**
+//		 * @author Dusan 
+//		 */
+//		String urlEduroam = "jdbc:jtds:sqlserver://192.168.77.230/psw-2018-tim7-1";
+//		String urlHome = "jdbc:jtds:sqlserver://147.91.175.155/psw-2018-tim7-1";
+//		String [] urls = {urlEduroam, urlHome};
+//		boolean stop_other_connections = false; //sluzi za proveru da li treba isprobavati dalje konektovanje
+//		Vector<Vector<Object>> valueMap = null;
+//		
+//		
+//		for (String url : urls) {
+//			try {
+//				conn = DriverManager.getConnection(url,"psw-2018-tim7-1","tim7-19940718");
+//				
+//				pstmt = conn.prepareStatement(sql);
+//				ResultSet rset = pstmt.executeQuery();
+//							
+//				valueMap = new Vector<Vector<Object>>();
+//				while(rset.next()) {
+//					
+//					Vector<Object> valueList = new Vector<Object>();
+//					for (int i = 1; i <= table.getAttributes().size(); i++) {
+//						Object object = rset.getObject(i);
+//						valueList.add(object);
+//					}
+//					valueMap.add(valueList);
+//				}
+//							
+//				rset.close();
+//				pstmt.close();
+//				stop_other_connections = true; 
+//			} catch (SQLException e) {
+//				String text = "<html>No such table <strong style=\"color: red;\">"+ table.getTitle() +"</strong> in database or no connection</html>";
+//				
+//				
+//				JOptionPane.showMessageDialog(null, text, "INVALID SQL", JOptionPane.ERROR_MESSAGE);
+//				
+//				if(url.contains("192.168.77.230")) {
+//					
+//					String message = "Do you want to connect to 147.91.175.155?";
+//					int response = JOptionPane.showConfirmDialog(null, message, "New route option", JOptionPane.YES_NO_OPTION);
+//					if(response == JOptionPane.YES_OPTION) {
+//						valueMap = null;
+//						continue;
+//					}
+//				}
+//				return null;
+//				
+//			}			
+//			if(stop_other_connections) {
+//				break;
+//			}
+//		}
+//		
+//		return valueMap;
 	}
 
 	@Override
