38d542a23292e414834f66c4cd521b1ac69e0e23
Dusan Svilarkovic <18049803+dsvilarkovic@users.noreply.github.com>
2019-01-08 14:49:51 +0000

    Uradjen test za dodavanje u bazu

--- URMA/test/handlerTest/AddRowDBTest.java
+++ URMA/test/handlerTest/AddRowDBTest.java
@@ -9,14 +9,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.MissingResourceException;
 import java.util.Vector;
 
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import controler.handlers.DBHandler;
 import controler.handlers.IHandler;
@@ -39,8 +38,6 @@ public class AddRowDBTest {
 
 	private static Connection conn = null;	
 	
-	@Rule  
-	public ExpectedException thrown = ExpectedException.none();
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -69,35 +66,15 @@ public class AddRowDBTest {
 	
 	
 	@Test(expected=NullPointerException.class)
+	/**
+	 * Testiranje, pozitivnog, tacnog unosa
+	 */
 	public void testPositiveAdd() {
 		Table table = DatabaseMockTable.createMockTable();
 		
 		
 		//napravi podatke
-		HashMap<String, Object> rowMap = new HashMap<>();
-		
-		
-		//ispuni podacima		
-		CharField charField = (CharField) new CharFieldFactory().createField();
-		charField.setValue("xyz");
-		VarcharField varcharField = (VarcharField) new VarcharFieldFactory().createField();
-		varcharField.setValue("mock_varchar");
-		IntegerField integerField = (IntegerField) new IntegerFieldFactory().createField();
-		integerField.setValue((Integer) 1);
-		DoubleField doubleField = (DoubleField) new DoubleFieldFactory().createField();
-		DoubleField copyDoubleField = spy(doubleField);
-		doReturn(",").when(copyDoubleField).splitChar();
-		copyDoubleField.setValue("1,25");
-		BooleanField booleanField = (BooleanField) new BooleanFieldFactory().createField();
-		booleanField.setValue((Boolean)true); 
-		DateField dateField = (DateField) new DateFieldFactory().createField();
-		
-		rowMap.put("MOCK_CHAR", charField);
-		rowMap.put("MOCK_VARCHAR", varcharField);
-		rowMap.put("MOCK_INT", integerField);
-		rowMap.put("MOCK_DOUBLE", copyDoubleField);
-		rowMap.put("MOCK_BOOLEAN", booleanField);
-		rowMap.put("MOCK_DATE", dateField);
+		HashMap<String, Object> rowMap = getRowMap();
 		
 		IHandler iHandler = new DBHandler();
 		//ubaci torku u tabelu
@@ -108,12 +85,10 @@ public class AddRowDBTest {
 		//procitaj celu tabelu
 		Vector<Vector<Object>> valueList = iHandler.read(table);
 		
-		
+		//iscitaj kodove kolona u tabeli	
 		List<String> columnCodes = DatabaseMockTable.getMockColumnHeaders(conn);// new ArrayList<>();
 		
-		
-		
-		
+				
 		boolean assertation = true;
 		for (int i = 0; i < valueList.size(); i++) {
 			Vector<Object> row = valueList.get(i);
@@ -126,6 +101,91 @@ public class AddRowDBTest {
 		assertEquals(true, assertation);
 	}
 	
+	@Test(expected=MissingResourceException.class)
+	/**
+	 * Testiranje pozitivnog ulaza, tj kad se prosledi null vrednost za neki od dozvoljenih
+	 */
+	public void testPositiveNullAdd() {
+		Table table = DatabaseMockTable.createMockTable();
+		
+		
+		//napravi podatke
+		HashMap<String, Object> rowMap = getRowMap();
+		
+		/**
+		 * Podesavanje null vrednosti za MOCK_DOUBLE, za koji je dozvoljeno da se podesi
+		 */
+		((DoubleField)rowMap.get("MOCK_DOUBLE")).setValue(null);
+		
+		IHandler iHandler = new DBHandler();
+		//ubaci torku u tabelu
+		
+		//when(App.INSTANCE.getTableMediator().showTable(table)).thenReturn();
+		iHandler.create(table, rowMap);
+		
+		//procitaj celu tabelu
+		Vector<Vector<Object>> valueList = iHandler.read(table);
+		
+		
+		
+		//iscitaj kodove kolona u tabeli	
+		List<String> columnCodes = DatabaseMockTable.getMockColumnHeaders(conn);// new ArrayList<>();
+		
+				
+		boolean assertation = true;
+		for (int i = 0; i < valueList.size(); i++) {
+			Vector<Object> row = valueList.get(i);
+			if(isThatRow(row, rowMap, columnCodes) == false){
+				assertation = false;
+				break;
+			}
+		}
+		
+		assertEquals(true, assertation);		
+	}
+	
+	@Test(expected=MissingResourceException.class)
+	/**
+	 * Testiranje negativnog ulaza, tj kad se prosledi null vrednost za kljuc
+	 */
+	public void testNegativeAdd() {
+		Table table = DatabaseMockTable.createMockTable();
+		
+		
+		//napravi podatke
+		HashMap<String, Object> rowMap = getRowMap();
+		
+		/**
+		 * Podesavanje null vrednosti za MOCK_DOUBLE, za koji je dozvoljeno da se podesi
+		 */
+		((CharField)rowMap.get("MOCK_CHAR")).setValue(null);
+		
+		IHandler iHandler = new DBHandler();
+		//ubaci torku u tabelu
+		
+		//when(App.INSTANCE.getTableMediator().showTable(table)).thenReturn();
+		iHandler.create(table, rowMap);
+		
+		//procitaj celu tabelu
+		Vector<Vector<Object>> valueList = iHandler.read(table);
+		
+		
+		
+		//iscitaj kodove kolona u tabeli	
+		List<String> columnCodes = DatabaseMockTable.getMockColumnHeaders(conn);// new ArrayList<>();
+		
+				
+		boolean assertation = true;
+		for (int i = 0; i < valueList.size(); i++) {
+			Vector<Object> row = valueList.get(i);
+			if(isThatRow(row, rowMap, columnCodes) == false){
+				assertation = false;
+				break;
+			}
+		}
+		
+		assertEquals(false, assertation);	
+	}
 	
 	private boolean isThatRow(Vector<Object> row, HashMap<String, Object> rowMap, List<String> columnsCode) {
 		boolean isThat = true;
@@ -142,6 +202,38 @@ public class AddRowDBTest {
 		return isThat;
 	}
 	
+	
+	/**
+	 * Pravljenje torke na osnovu fiktivne forme
+	 * @return
+	 */
+	private HashMap<String, Object> getRowMap(){
+		HashMap<String, Object> rowMap = new HashMap<>();
+		//ispuni podacima		
+		CharField charField = (CharField) new CharFieldFactory().createField();
+		charField.setValue("xyz");
+		VarcharField varcharField = (VarcharField) new VarcharFieldFactory().createField();
+		varcharField.setValue("mock_varchar");
+		IntegerField integerField = (IntegerField) new IntegerFieldFactory().createField();
+		integerField.setValue((Integer) 1);
+		DoubleField doubleField = (DoubleField) new DoubleFieldFactory().createField();
+		DoubleField copyDoubleField = spy(doubleField);
+		doReturn(",").when(copyDoubleField).splitChar();
+		copyDoubleField.setValue("1,25");
+		BooleanField booleanField = (BooleanField) new BooleanFieldFactory().createField();
+		booleanField.setValue((Boolean)true); 
+		DateField dateField = (DateField) new DateFieldFactory().createField();
+				
+		rowMap.put("MOCK_CHAR", charField);
+		rowMap.put("MOCK_VARCHAR", varcharField);
+		rowMap.put("MOCK_INT", integerField);
+		rowMap.put("MOCK_DOUBLE", copyDoubleField);
+		rowMap.put("MOCK_BOOLEAN", booleanField);
+		rowMap.put("MOCK_DATE", dateField);
+
+		
+		return rowMap;
+	}
 	
 	
 	
